6443497109b782f985f9b26fa2dc563c35d3effa
changawa-antony <antony.changawa@student.moringaschool.com>
2021-06-15 10:55:41 +0300

    updated the view template

--- app/views.py
+++ app/views.py
@@ -13,3 +13,14 @@ def index():
     title = "One min pitch"
     return render_template('index.html', title = title)
 
+@app.route('/login')
+def login():
+    
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    title = "One min pitch"
+    return render_template('login.html', title = title)
+
